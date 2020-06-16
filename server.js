c77d14d92344c924759586761c58ef16a1294fab
Mark Mavromatis <markmavromatis@Marks-2020-Macbook-Pro-13.local>
2020-06-16 15:32:39 -0500

    Added env variable reference and travis code

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log("ENV variable PRINT_THIS = " + process.env.PRINT_THIS)
     await sleep(5000);
   }
 }
